1f1ae6e7748f2ab4b8973b4261add046666d26ba
Gbolade Idowu <idowugboladeshem@gmail.com>
2024-04-12 03:50:34 +0100

    Added Issuance entity and created APIs

--- src/main/java/com/example/stockkeeping/entity/Issuance.java
+++ src/main/java/com/example/stockkeeping/entity/Issuance.java
@@ -3,7 +3,6 @@ package com.example.stockkeeping.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
-import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 
