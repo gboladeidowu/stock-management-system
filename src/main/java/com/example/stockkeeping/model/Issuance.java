1ed785a40306db16784b8624726299477ecd25ad
gboladeidowu <idowugboladeshem@gmail.com>
2024-08-08 01:07:35 +0100

    update

--- src/main/java/com/example/stockkeeping/model/Issuance.java
+++ src/main/java/com/example/stockkeeping/model/Issuance.java
@@ -2,27 +2,37 @@ package com.example.stockkeeping.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 @Entity
 @Table(name = "issuance")
 public class Issuance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     @NotEmpty
     private String itemCode;
     @NotEmpty
     private String itemName;
+    @NotEmpty
+    private String itemDescription;
+
     private int issuedQuantity;
     @NotEmpty
     private String station;
     @NotEmpty
     private String issuer;
-    private LocalDate issuedDate = LocalDate.now();
+    private LocalDate issuedDate;
+
 
 
 
